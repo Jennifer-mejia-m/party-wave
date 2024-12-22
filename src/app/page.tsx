143509c5a850cc0a3dae962a1bc720d75dbc485e
Jennifer Mejia Meliton <jenniux55@gmail.com>
2024-12-22 10:32:22 -0500

    change title color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,8 @@ const HomePage: React.FC = () => {
 
   return (
     <div className='max-w-4xl mx-auto p-6'>
-      <h1 className='text-3xl font-bold text-white mb-6 text-center'>
-        Party Wave
+      <h1 className='text-3xl font-bold text-black mb-6 text-center'>
+        Party Wave Jennifer
       </h1>
       <SearchBar onSearch={handleSearch} />
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
